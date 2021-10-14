30764abdea63b59e219a39fc65b08988abbf4ecc
Sheethal03 <90698417+Sheethal03@users.noreply.github.com>
2021-10-14 20:20:09 +0530

    created built-in-function
    this file to find size of built in functions

---
+++ hello.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  char c;
+  int a;
+  float f;
+  double d;
+  printf("size of Charcter=%d bytes\n",sizeof(c));
+  printf("size of Integer=%d bytes\n",sizeof(a));
+  printf("size of Float=%d bytes\n",sizeof(f));
+  printf("size of double=%d bytes\n",sizeof(d));
+  getch;
+  }
